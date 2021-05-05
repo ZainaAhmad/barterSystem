b6987156c1c17716798d73f340a3779041071d49
ZainaAhmad <63003618+ZainaAhmad@users.noreply.github.com>
2021-05-05 18:27:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import WelcomeScreen from './screens/welcomeScreen';
+
+export default function App() {
+  return (
+    <View>
+      <WelcomeScreen/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
